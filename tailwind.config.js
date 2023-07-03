6ecf46bb250ea8df29e844a99fb5b94b342da1df
Favour-Yusuf <favouryusuf45@gmail.com>
2023-07-03 15:22:59 +0100

    hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {
-      'hero-pattern': "url('https://images.pexels.com/photos/1907785/pexels-photo-1907785.jpeg?auto=compress&cs=tinysrgb&w=600')",
+      'hero-pattern': "url('https://www.pexels.com/es-es/foto/1907785/')",
     },
   },
   plugins: [],
